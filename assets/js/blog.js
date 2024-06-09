6dc3b9b5591d2a50aebda1b0b38df13fca4d3f1e
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-06-09 10:39:28 -0500

    Updated Blog Content and view

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,16 +1,16 @@
-//
+// creates and event where locally sotred information is applied to the a new div tag and stored on the page in new divs every time new information is submitted on the submitforms page
 document.addEventListener("DOMContentLoaded", function () {
-  const postsContainer = document.getElementById("formData");
-  const storedPosts = JSON.parse(localStorage.getItem("formData")) || [];
-  //for loop
+  const postsContainer = document.getElementById("posts");
+  const storedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+  // For loop
   for (let i = 0; i < storedPosts.length; i++) {
     const postData = storedPosts[i];
     const postElement = document.createElement("div");
     postElement.className = "post";
     postElement.innerHTML = `
-            <h3>${postData.title}</h3>
+            <h2>${postData.title}</h2>            
+            <p>${postData.content}</p>
             <p>Username: ${postData.username}</p>
-            <p>Content: ${postData.content}</p>
         `;
     postsContainer.appendChild(postElement);
   }
